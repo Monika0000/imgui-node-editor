11a255e0e47796f9545e66f344af2e7916a24a72
innerviewer <innerviewerr@gmail.com>
2023-01-18 21:17:05 +0100

    Fixed MSVC compilation errors

--- imgui_node_editor.cpp
+++ imgui_node_editor.cpp
@@ -2267,7 +2267,7 @@ void ed::EditorContext::UpdateAnimations()
     }
 }
 
-void ed::EditorContext::Flow(Link* link, FlowDirection direction)
+void ed::EditorContext::Flow(Link* link, ax::NodeEditor::FlowDirection direction)
 {
     m_FlowAnimationController.Flow(link, direction);
 }
@@ -3205,7 +3205,7 @@ ed::FlowAnimationController::~FlowAnimationController()
         delete animation;
 }
 
-void ed::FlowAnimationController::Flow(Link* link, FlowDirection direction)
+void ed::FlowAnimationController::Flow(Link* link, ax::NodeEditor::FlowDirection direction)
 {
     if (!link || !link->m_IsLive)
         return;
